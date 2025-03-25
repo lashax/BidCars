2e0c1b6a471ca6fffaab68bbe4aaa4e06400d465
Lasha Sherozia <lasha@Lashas-MacBook-Air.local>
2025-03-25 22:53:23 +0400

    Initial commit

---
+++ src/main/java/io/github/lashax/bidcars/BidCarsApplication.java
@@ -0,0 +1,13 @@
+package io.github.lashax.bidcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lashax/bidcars/BidCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lashax.bidcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
